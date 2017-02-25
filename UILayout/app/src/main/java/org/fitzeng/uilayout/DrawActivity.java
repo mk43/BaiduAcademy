a2dcae60c552557e9fbfe90a4dc2838a7f055434
Fitzeng <1801598925@qq.com>
2017-02-25 20:47:28 +0800

    init UILayout

---
+++ UILayout/app/src/main/java/org/fitzeng/uilayout/DrawActivity.java
@@ -0,0 +1,13 @@
+package org.fitzeng.uilayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DrawActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_draw);
+    }
+}
